18c538159a42cfc5f2b29da8b88b56399fef9a7c
shrutika2611 <shrutikasatpute26@gmail.com>
2024-11-27 01:48:29 +0530

    Create Maxima5.py

---
+++ Maxima5.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+num4 = float(input("Enter the fourth number: "))
+num5 = float(input("Enter the fifth number: "))
+
+
+total_sum = num1 + num2 + num3 + num4 + num5
+
+
+average = total_sum / 5
+
+
+print(f"The average of the five numbers is: {average}")
